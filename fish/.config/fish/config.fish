bbbe63079d4318ed11918c60edb0d26e5bbe95ee
Emil Jansson <emil.jansson@compileit.com>
2025-11-07 14:01:57 +0100

    🔧 use fish in ghostty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
+/opt/homebrew/bin/brew shellenv | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
+    fnm env --use-on-cd --shell fish --corepack-enabled --version-file-strategy recursive | source
 end
