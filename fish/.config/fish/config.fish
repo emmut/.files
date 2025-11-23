56d11e7bb88f4635c83eb2e4ff75292a51a195d5
Emil Jansson <e.jansson@icloud.com>
2025-11-23 15:11:36 +0100

    🔧 fixes for brew and fnm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,17 @@
-/opt/homebrew/bin/brew shellenv | source
+if test -f /opt/homebrew/bin/brew
+    /opt/homebrew/bin/brew shellenv | source
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
-    fnm env --use-on-cd --shell fish --corepack-enabled --version-file-strategy recursive | source
+
+    # fnm environment setup
+    if test -d ~/.fnm
+        set -gx PATH "$HOME/.fnm:$PATH"
+        if command -v fnm >/dev/null 2>&1
+            fnm env --use-on-cd --shell fish --corepack-enabled --version-file-strategy recursive | source
+        end
+    end
 end
