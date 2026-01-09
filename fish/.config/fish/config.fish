709903006960c341bf2d2c9d6185d0c2a077188b
Emil Jansson <emil.jansson@compileit.com>
2026-01-09 18:48:48 +0100

    Revert "🔧 fixes for brew and fnm"
    This reverts commit 56d11e7bb88f4635c83eb2e4ff75292a51a195d5.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,8 @@
-if test -f /opt/homebrew/bin/brew
-    /opt/homebrew/bin/brew shellenv | source
-end
+/opt/homebrew/bin/brew shellenv | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
-
-    # fnm environment setup
-    if test -d ~/.fnm
-        set -gx PATH "$HOME/.fnm:$PATH"
-        if command -v fnm >/dev/null 2>&1
-            fnm env --use-on-cd --shell fish --corepack-enabled --version-file-strategy recursive | source
-        end
-    end
+    fnm env --use-on-cd --shell fish --corepack-enabled --version-file-strategy recursive | source
 end
